8264c41ef25a2a8de03280debe2ffa2fb35276c4
Marco-S117 <polino47@gmail.com>
2020-01-25 15:57:47 +0100

    Update welcome.blade.php
    PRova 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,6 +87,7 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laravel.com/docs">Modifica senza consenso</a>
+                    <a href="https://laravel.com/docs">Prova di Modifica senza consenso</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">COAP</a>
                     <a href="https://blog.laravel.com">Blog</a>
